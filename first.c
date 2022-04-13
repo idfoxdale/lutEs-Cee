7a9990779e1e9257ecfb0a45b77c2698271f7532
ishan <ishantkdas@gmail.com>
2022-04-13 15:27:42 +0530

    C commit one

---
+++ first.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+This is multi
+line comments
+*/
+int main(){
+    //comment
+    int a = 3;
+    float b = 8.5;
+    char c = 'u';
+    printf("Hello This is Ishant \n");
+    printf("The value of a is %d. \n", a);
+    printf("The value of b is %f. \n", b);
+    printf("The value of a is %c. \n", c);
+    return 0;
+}
